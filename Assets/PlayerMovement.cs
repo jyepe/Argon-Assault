bc7eaf2cfb44383d511aecac0832811c48f1fad6
jyepe <yepejy@gmail.com>
2018-03-27 13:49:02 -0400

    Ship can move on x and y axis

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,13 +5,50 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    [Tooltip("In ms^-1")] [SerializeField] private float xSpeed = 4f;   //Speed at which the ship will move along x-axis
+    [Tooltip("In ms^-1")] [SerializeField] private float ySpeed = 4f;   //Speed at which the ship will move along y-axis
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-	}
+	void Update ()
+    {
+        MovementOnXAxis();
+        MovementOnYAxis();
+    }
+
+    private void MovementOnYAxis()
+    {
+        //Value between -1 and 1. GetAxis returns the value of the virtual axis. Value changes when you press
+        //W, S, UpArrow, DownArrow
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        //Calculate where on the y-axis the ship should be. Time.delta time makes it frame rate independent
+        float yOffset = ySpeed * yThrow * Time.deltaTime;
+
+        //The new y position of the ship
+        float newYPosition = yOffset + transform.localPosition.y;
+
+        //Moves the ship
+        transform.localPosition = new Vector3(transform.localPosition.x, newYPosition, transform.localPosition.z);
+    }
+
+    private void MovementOnXAxis()
+    {
+        //Value between -1 and 1. GetAxis returns the value of the virtual axis. Value changes when you press
+        //A, D, <--, -->
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+
+        //Calculate where on the x-axis the ship should be. Time.delta time makes it frame rate independent
+        float xOffset = xSpeed * xThrow * Time.deltaTime;
+
+        //The new x position of the ship
+        float newXPosition = xOffset + transform.localPosition.x;
+
+        //Moves the ship
+        transform.localPosition = new Vector3(newXPosition, transform.localPosition.y, transform.localPosition.z);
+    }
 }
