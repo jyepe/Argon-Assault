b2e0192657574801ea6d1f30d1023abc2f22eede
jyepe <yepejy@gmail.com>
2018-03-27 01:21:30 -0400

    Space skybox added

---
+++ Assets/StartGame.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Invoke("Play", 5f);
+	}
+
+    private void Play()
+    {
+        SceneManager.LoadScene(getCurrentLevel() + 1);
+    }
+
+    private int getCurrentLevel()
+    {
+        return SceneManager.GetActiveScene().buildIndex;
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    // Update is called once per frame
+    void Update () {
+        
+	}
+}
