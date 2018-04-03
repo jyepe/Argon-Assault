81f9d80fb07176f044f5edc2c8825fef9b17a218
jyepe <yepejy@gmail.com>
2018-04-03 01:20:47 -0400

    Enemy ships blow up

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
+    [SerializeField] GameObject explosion;
+
 	// Use this for initialization
 	void Start () {
         BoxCollider enemyCollider = gameObject.AddComponent<BoxCollider>();
@@ -17,6 +19,7 @@ public class Enemy : MonoBehaviour {
 
     private void OnParticleCollision(GameObject other)
     {
+        Instantiate(explosion, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
 }
