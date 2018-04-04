86e6f316c4332ac22c6f6010dc5d3dabbfcdde81
jyepe <yepejy@gmail.com>
2018-04-04 01:28:22 -0400

    UI features added
    Main menu prompts uder to press p to start game. Also a score is kept in bottom left corner

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -7,25 +7,18 @@ public class StartGame : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Invoke("Play", 5f);
 	}
-
-    private void Play()
-    {
-        SceneManager.LoadScene(1);
-    }
-
-    private int getCurrentLevel()
-    {
-        return SceneManager.GetActiveScene().buildIndex;
-    }
 
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
     }
+
     // Update is called once per frame
     void Update () {
-        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            SceneManager.LoadScene(1);
+        }
 	}
 }
