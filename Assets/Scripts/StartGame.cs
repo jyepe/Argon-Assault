a7c9c0db13ac6cc962c76a2041aecf0d94eb7efe
jyepe <yepejy@gmail.com>
2018-04-14 21:12:33 -0400

    New main menu added

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -5,15 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	}
-
-    // Update is called once per frame
-    void Update () {
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            SceneManager.LoadScene(1);
-        }
-	}
+	public void startGame(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
 }
